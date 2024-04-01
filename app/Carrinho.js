ecb4c0b24b039205666938c2f726fbac8a6748fe
rafaellabastos <rafa.bastos@yahoo.com.br>
2024-04-01 11:30:01 -0300

    carrinho pré-pronto

---
+++ app/Carrinho.js
@@ -0,0 +1,20 @@
+import { View, Text } from 'react-native';
+
+export default function Agendar() {
+    return(
+        <>
+            <View>
+                <Text>Carrinho</Text>
+            </View>
+
+            <View>
+                <Image source={}/>
+                <Text>Alecrim dourado</Text>
+                <Text>R$16,00</Text>
+
+                <Text>Ver mais</Text>
+                <Text>Excluir]</Text>
+            </View>
+        </>
+    )
+}
