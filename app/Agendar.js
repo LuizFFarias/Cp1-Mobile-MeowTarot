61a3ac0f76b17fd51604e5a37985a5cce882bde7
rafaellabastos <rafa.bastos@yahoo.com.br>
2024-04-02 08:39:22 -0300

    estilização da página de agendar

--- app/Agendar.js
+++ app/Agendar.js
@@ -1,55 +1,137 @@
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+
+const styles = StyleSheet.create({
+    subtitulo: {
+        backgroundColor: '#FB76D6',
+        borderTopLeftRadius: 0,
+        borderTopRightRadius: 10,
+        borderBottomLeftRadius: 0,
+        borderBottomRightRadius: 10,
+        height: 83,
+        width: 459,
+        marginTop: 30,
+
+    },
+    textoSub: {
+        textAlign: 'left',
+        marginLeft: 7,
+        fontSize: 22,
+    },
+
+    card: {
+        backgroundColor: '#E8E0E5',
+        height: 203,
+        width: 634,
+    },
+    cardImg: {
+        height: 167,
+        width: 167,
+        alignSelf: 'left',
+        marginTop: 10,
+        marginLeft: 10,
+    },
+    cardTit: {
+        textAlign: 'left',
+        marginLeft: 15,
+        fontSize: 17,
+    },
+    cardPreco: {
+        textAlign: 'left',
+        marginLeft: 1,
+        fontSize: 17,
+    },
+
+    botao: {
+        backgroundColor: '#1E9C09',
+        borderTopLeftRadius: 5,
+        borderTopRightRadius: 5,
+        borderBottomLeftRadius: 5,
+        borderBottomRightRadius: 5,
+        height: 31,
+        width: 139,
+    },
+    botaoText: {
+        textAlign: 'center',
+        fontSize: 11,
+    }
+});
 
 export default function Agendar() {
     return(
         <>
-            <View>
-                <Text>Agende um atendimento</Text>
+            <View style={styles.subtitulo}>
+                <Text style={styles.textoSub}>Agende um atendimento</Text>
             </View>
 
-            <View>
-                <Image source={}/>
-                <Text>Alecrim dourado</Text>
-                <Text>R$16,00</Text>
+            <View style={styles.card}>
+                <Image source={} style={styles.cardImg}/>
+                <Text style={styles.cardTit}>Alecrim dourado</Text>
+                <Text style={styles.cardPreco}>R$16,00</Text>
 
-                <Text>Ver mais</Text>
-                <Text>Adicionar</Text>
+                <TouchableOpacity styles={styles.botao}>
+                    <Text styles={styles.botaoText}>Ver mais</Text> 
+                </TouchableOpacity>
+                
+                <TouchableOpacity styles={styles.botao}>
+                    <Text styles={styles.botaoText}>Adicionar</Text>
+                </TouchableOpacity>
             </View>
 
-            <View>
-                <Image source={}/>
-                <Text>Perguntas avulsas</Text>
-                <Text>R$10,00</Text>
+            <View style={styles.card}>
+                <Image source={} style={styles.cardImg}/>
+                <Text style={styles.cardTit}>Perguntas avulsas</Text>
+                <Text style={styles.cardPreco}>R$10,00</Text>
 
-                <Text>Ver mais</Text>
-                <Text>Adicionar</Text>
+                <TouchableOpacity styles={styles.botao}>
+                    <Text styles={styles.botaoText}>Ver mais</Text> 
+                </TouchableOpacity>
+                
+                <TouchableOpacity styles={styles.botao}>
+                    <Text styles={styles.botaoText}>Adicionar</Text>
+                </TouchableOpacity>
+                
             </View>
 
-            <View>
-                <Image source={}/>
-                <Text>Leitura da estrela</Text>
-                <Text>R$32,00</Text>
+            <View style={styles.card}>
+                <Image source={} style={styles.cardImg}/>
+                <Text style={styles.cardTit}>Leitura da estrela</Text>
+                <Text style={styles.cardPreco}>R$32,00</Text>
 
-                <Text>Ver mais</Text>
-                <Text>Adicionar</Text>
+                <TouchableOpacity styles={styles.botao}>
+                    <Text styles={styles.botaoText}>Ver mais</Text> 
+                </TouchableOpacity>
+                
+                <TouchableOpacity styles={styles.botao}>
+                    <Text styles={styles.botaoText}>Adicionar</Text>
+                </TouchableOpacity>
             </View>
 
-            <View>
-                <Image source={}/>
-                <Text>Conselho</Text>
-                <Text>R$4,00</Text>
+            <View style={styles.card}>
+                <Image source={} style={styles.cardImg}/>
+                <Text style={styles.cardTit}>Conselho</Text>
+                <Text style={styles.cardPreco}>R$4,00</Text>
 
-                <Text>Ver mais</Text>
-                <Text>Adicionar</Text>
+                <TouchableOpacity styles={styles.botao}>
+                    <Text styles={styles.botaoText}>Ver mais</Text> 
+                </TouchableOpacity>
+                
+                <TouchableOpacity styles={styles.botao}>
+                    <Text styles={styles.botaoText}>Adicionar</Text>
+                </TouchableOpacity>
             </View>
 
-            <View>
-                <Image source={}/>
-                <Text>Leitura da corte</Text>
-                <Text>R$34,00</Text>
+            <View style={styles.card}>
+                <Image source={} style={styles.cardImg}/>
+                <Text style={styles.cardTit}>Leitura da corte</Text>
+                <Text style={styles.cardPreco}>R$34,00</Text>
 
-                <Text>Ver mais</Text>
-                <Text>Adicionar</Text>
+                <TouchableOpacity styles={styles.botao}>
+                    <Text styles={styles.botaoText}>Ver mais</Text> 
+                </TouchableOpacity>
+                
+                <TouchableOpacity styles={styles.botao}>
+                    <Text styles={styles.botaoText}>Adicionar</Text>
+                </TouchableOpacity>
             </View>
         </>
     )
